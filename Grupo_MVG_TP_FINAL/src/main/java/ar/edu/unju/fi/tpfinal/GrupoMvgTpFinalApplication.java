8843cbe4b45fae097f47263c44a3d2536b00051f
Gallardo <gallardojoako@gmail.com>
2021-06-11 18:48:49 -0300

    Primer comit

---
+++ Grupo_MVG_TP_FINAL/src/main/java/ar/edu/unju/fi/tpfinal/GrupoMvgTpFinalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoMvgTpFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoMvgTpFinalApplication.class, args);
+	}
+
+}
