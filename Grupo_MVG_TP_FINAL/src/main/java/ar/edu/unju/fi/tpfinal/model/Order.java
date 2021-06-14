41d4ae0208286a0234c96820b8a8f2e2ae8b2bc1
sofia-munoz <sofim.2803.1@gmail.com>
2021-06-14 20:19:36 -0300

    Agrego el application.properties

--- Grupo_MVG_TP_FINAL/src/main/java/ar/edu/unju/fi/tpfinal/model/Order.java
+++ Grupo_MVG_TP_FINAL/src/main/java/ar/edu/unju/fi/tpfinal/model/Order.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
