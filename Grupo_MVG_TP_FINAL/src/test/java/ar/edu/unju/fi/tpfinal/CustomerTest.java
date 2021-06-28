c8af2f4c5c880e804079e2f2afc22e1f92f1b64c
FlorNV <florencianoelivaldez@gmail.com>
2021-06-28 14:07:47 -0300

    Agrego archivo de configuracion para spring security

--- Grupo_MVG_TP_FINAL/src/test/java/ar/edu/unju/fi/tpfinal/CustomerTest.java
+++ Grupo_MVG_TP_FINAL/src/test/java/ar/edu/unju/fi/tpfinal/CustomerTest.java
@@ -33,21 +33,21 @@ class CustomerTest {
 			
 			Optional<Employee> empleado2 = employeeService.getEmployeePorId(5);
 			
-			Customer cliente1 = new Customer(Long.valueOf(1), "Atelier graphique","Schmitt", "Carine ", "40.32.2555", "54, rue Royale", null, "Nantes", null, "4400", "France", BigDecimal.valueOf(1370), empleado1.get());
+			Customer cliente1 = new Customer(Long.valueOf(1), "Atelier graphique","Schmitt", "Carine ", "40.32.2555", "54, rue Royale", null, "Nantes", null, "4400", "France", BigDecimal.valueOf(1370), empleado1.get(),null);
 			customerService.guardarCustomer(cliente1);
 			
 			System.out.println(cliente1);
 			
-			Customer cliente2 = new Customer(Long.valueOf(2), "Signal Gift Stores","King", "Jean ", "7025551838", "8489 Strong St.", null, "Las Vegas", "NV", "83030", "USA", BigDecimal.valueOf(71800.00), empleado2.get());
+			Customer cliente2 = new Customer(Long.valueOf(2), "Signal Gift Stores","King", "Jean ", "7025551838", "8489 Strong St.", null, "Las Vegas", "NV", "83030", "USA", BigDecimal.valueOf(71800.00), empleado2.get(),null);
 			customerService.guardarCustomer(cliente2);
 			
-			Customer cliente3 = new Customer(Long.valueOf(3), "Australian Collectors, Co.","Ferguson", "Peter ", "03 9520 4555", "636 St Kilda Road", "Level 3", "Melbourne", "Melbourne", "3004", "Australia", BigDecimal.valueOf(117300.00), empleado2.get());
+			Customer cliente3 = new Customer(Long.valueOf(3), "Australian Collectors, Co.","Ferguson", "Peter ", "03 9520 4555", "636 St Kilda Road", "Level 3", "Melbourne", "Melbourne", "3004", "Australia", BigDecimal.valueOf(117300.00), empleado2.get(),null);
 			customerService.guardarCustomer(cliente3);
 			
-			Customer cliente4 = new Customer(Long.valueOf(4), "La Rochelle Gifts","Labrune", "Janine  ", "40.67.8555", "67, rue des Cinquante Otages", null, "Nantes", null, "4400", "France", BigDecimal.valueOf(118200.00), empleado1.get());
+			Customer cliente4 = new Customer(Long.valueOf(4), "La Rochelle Gifts","Labrune", "Janine  ", "40.67.8555", "67, rue des Cinquante Otages", null, "Nantes", null, "4400", "France", BigDecimal.valueOf(118200.00), empleado1.get(),null);
 			customerService.guardarCustomer(cliente4);
 			
-			Customer cliente5 = new Customer(Long.valueOf(5), "Baane Mini Imports","Bergulfsen", "Jonas  ", "07-98 9555", "Erling Skakkes gate 78", null, "Stavern", null, "4110", "Norway", BigDecimal.valueOf(81700.00), empleado1.get());
+			Customer cliente5 = new Customer(Long.valueOf(5), "Baane Mini Imports","Bergulfsen", "Jonas  ", "07-98 9555", "Erling Skakkes gate 78", null, "Stavern", null, "4110", "Norway", BigDecimal.valueOf(81700.00), empleado1.get(),null);
 			customerService.guardarCustomer(cliente5);
 			
 			assertEquals(1,cliente1.getIdCliente());
--- Grupo_MVG_TP_FINAL/src/test/java/ar/edu/unju/fi/tpfinal/EmployeeTest.java
+++ Grupo_MVG_TP_FINAL/src/test/java/ar/edu/unju/fi/tpfinal/EmployeeTest.java
@@ -28,7 +28,7 @@ class EmployeeTest {
 		Optional<Office> oficina2 = officeService.getOfficePorId("5");
 		Optional<Office> oficina3 = officeService.getOfficePorId("4");
 		
-		Employee employee1 = new Employee(Integer.valueOf(1), "Murphy", "Diane", "x5800", "dmurphy@classicmodelcars.com", "President", oficina1.get(), null);
+		Employee employee1 = new Employee(Integer.valueOf(1), "Murphy", "Diane", "x5800", "dmurphy@classicmodelcars.com", "President", oficina1.get(), null, null);
 		employeeService.guardarEmployee(employee1);
 		
 		System.out.println(employee1);
@@ -37,18 +37,18 @@ class EmployeeTest {
 		
 		Optional<Employee> superior1 = employeeService.getEmployeePorId(1);
 		
-		Employee employee2 = new Employee(Integer.valueOf(2), "Patterson", "Mary", "x4611", "mpatterso@classicmodelcars.com", "VP Sales", oficina1.get(),superior1.get());
+		Employee employee2 = new Employee(Integer.valueOf(2), "Patterson", "Mary", "x4611", "mpatterso@classicmodelcars.com", "VP Sales", oficina1.get(),superior1.get(), null);
 		employeeService.guardarEmployee(employee2);
 		
 		System.out.println("Superior: " + employee2.getSuperior().getId());
 		
-		Employee employee3 = new Employee(Integer.valueOf(3), "Firrelli", "Jeff", "x9273", "jfirrelli@classicmodelcars.com", "VP Marketing", oficina1.get(), superior1.get());
+		Employee employee3 = new Employee(Integer.valueOf(3), "Firrelli", "Jeff", "x9273", "jfirrelli@classicmodelcars.com", "VP Marketing", oficina1.get(), superior1.get(), null);
 		employeeService.guardarEmployee(employee3);
 		
-		Employee employee4 = new Employee(Integer.valueOf(4), "Patterson", "William", "x4871", "wpatterson@classicmodelcars.com", "Sales Manager (APAC)", oficina2.get(), employee2);
+		Employee employee4 = new Employee(Integer.valueOf(4), "Patterson", "William", "x4871", "wpatterson@classicmodelcars.com", "Sales Manager (APAC)", oficina2.get(), employee2, null);
 		employeeService.guardarEmployee(employee4);
 		
-		Employee employee5 = new Employee(Integer.valueOf(5), "Bondur", "Gerard", "x5408", "gbondur@classicmodelcars.com", "Sale Manager (EMEA)", oficina3.get(), employee2);
+		Employee employee5 = new Employee(Integer.valueOf(5), "Bondur", "Gerard", "x5408", "gbondur@classicmodelcars.com", "Sale Manager (EMEA)", oficina3.get(), employee2, null);
 		employeeService.guardarEmployee(employee5);
 		
 		assertEquals("Murphy",employee1.getApellido());
